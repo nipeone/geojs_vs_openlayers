900e2c8a39b9bc3b078e5844e9f9e2c9179ca141
nipeone <x0601y@live.com>
2024-05-22 01:07:25 +0800

    use webgl vector layer

--- scripts/ol.js
+++ scripts/ol.js
@@ -4,30 +4,36 @@ import OSM from 'ol/source/OSM.js';
 import VectorSource from 'ol/source/Vector.js';
 import VectorImageLayer from 'ol/layer/VectorImage.js';
 import GeoJSON from 'ol/format/GeoJSON.js';
-import { transform } from 'ol/proj.js'
+import { transform, fromLonLat } from 'ol/proj.js'
 import { Style, Stroke, Fill } from 'ol/style.js';
-import Layer from 'ol/layer/Layer.js'
-import TileLayer from 'ol/layer/Tile.js'
+import Layer from 'ol/layer/Layer.js';
+// import TileLayer from 'ol/layer/Tile.js'
+import TileLayer from 'ol/layer/WebGLTile.js';
 import WebGLVectorLayerRenderer from 'ol/renderer/webgl/VectorLayer.js';
 
 import { renderer, geojsonPath, getData } from './common.js'
 
-var styles = [
-  new Style({
-    stroke: new Stroke({
-      color: 'blue',
-      width: 3
-    }),
-    fill: new Fill({
-      color: 'rgba(0, 0, 255, 0.1)'
-    })
-  })
-];
+// const style = new Style({
+//     stroke: new Stroke({
+//       color: 'blue',
+//       width: 3
+//     }),
+//     fill: new Fill({
+//       color: 'rgba(0, 0, 255, 0.1)'
+//     })
+// });
+
+const style = {
+  'stroke-color': 'blue',
+  'stroke-width': 3,
+  'fill-color': 'rgba(0, 0, 255, 0.1)',
+}
 
 class WebGLLayer extends Layer {
   createRenderer() {
     return new WebGLVectorLayerRenderer(this, {
-      style: styles,
+      className: this.getClassName(),
+      style: style,
     });
   }
 }
@@ -35,36 +41,30 @@ class WebGLLayer extends Layer {
 var osmLayer = new TileLayer({
   source: new OSM()
 });
-var featureLayer = new VectorImageLayer({
-  style: styles,
-  renderBuffer: 90000,
-  source: new VectorSource({
-    features: (new GeoJSON()).readFeatures(await getData(), { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857' })
-  }),
-});
 
-// var featureLayer = new VectorImageLayer({
-//   renderMode: 'image',
-//   style: styles,
+// var featureLayer = new WebGLLayer({
 //   source: new VectorSource({
-//     format: new GeoJSON(),
-//     url: geojsonPath,
+//     features: (new GeoJSON()).readFeatures(await getData(), { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857' })
 //   }),
 // });
 
+var featureLayer = new WebGLLayer({
+  source: new VectorSource({
+    format: new GeoJSON(),
+    url: geojsonPath,
+  }),
+});
+
 // Use the "webgl" renderer by default.
 var map = new Map({
-  renderer: renderer,
   layers: [
     osmLayer, 
     featureLayer
   ],
   target: document.getElementById('map'),
   view: new View({
-    // center: transform([-88.30, 42.015], 'EPSG:4326', 'EPSG:3857'),
-    center: transform([105.77, 35.84], 'EPSG:4326', 'EPSG:3857'),
+    // center: transform([105.77, 35.84], 'EPSG:4326', 'EPSG:3857'),
+    center: fromLonLat([105.77, 35.84]),
     zoom: 9
   })
 });
-
-
